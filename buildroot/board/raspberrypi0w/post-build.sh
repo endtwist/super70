bcd80130182eea7eba230676080d71fad43dd908
Joshua Gross <joshua@gross.is>
2018-07-01 16:43:08 -0400

    Completely replace GENERIC_SERIAL terminal with HDMI only

--- buildroot/board/raspberrypi0w/post-build.sh
+++ buildroot/board/raspberrypi0w/post-build.sh
@@ -8,8 +8,7 @@ BOARD_DIR="$(dirname $0)"
 # Add a console on tty1
 if [ -e ${TARGET_DIR}/etc/inittab ]; then
     grep -qE '^tty1::' ${TARGET_DIR}/etc/inittab || \
-	sed -i '/GENERIC_SERIAL/a\
-tty1::respawn:/sbin/getty -L  tty1 0 vt100 # HDMI console' ${TARGET_DIR}/etc/inittab
+	sed -i 's/^.*GENERIC_SERIAL$/tty1::respawn:\/sbin\/getty -L  tty1 0 vt100 # HDMI console/' ${TARGET_DIR}/etc/inittab
 fi
 
 # Add SD card automount
