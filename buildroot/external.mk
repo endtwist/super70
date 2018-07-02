ba2cac307e780f8edf23b47e595bb5dd13b9519c
Joshua Gross <joshua@gross.is>
2018-07-02 10:38:11 -0400

    Swap package makefile for a var export in the external.mk

--- buildroot/external.mk
+++ buildroot/external.mk
@@ -1,1 +1,4 @@
-include $(sort $(wildcard $(BR2_EXTERNAL_SUPER70_PATH)/package/*/*.mk))
+# Add missing build vars required to properly build kmod with python3
+KMOD_CONF_ENV += \
+	PYTHON_LIBS="`$(STAGING_DIR)/usr/bin/python3-config --ldflags`" \
+	PYTHON_CFLAGS="`$(STAGING_DIR)/usr/bin/python3-config --cflags`"
